dfe764d0dcc413237b5ad0edb19e8f255f52994a
nikkinbird <nikki.bollman@gmail.com>
2021-06-20 10:01:26 -0500

    Created global variables

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const guessedLetters = document.querySelector(".guessed-letters");
+const guessButton = document.querySelector(".guess");
+const letter = document.querySelector(".letter");
+const wordInProgress = document.querySelector(".word-in-progress");
+const remaining = document.querySelector(".remaining");
+const remainingSpan = document.querySelector(".remaining span");
+const message = document.querySelector(".message");
+const playAgainButton = document.querySelector(".play-again");
+const word = "magnolia";
